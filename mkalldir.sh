fc8fc2e09905d566b657fac38cc9fe73938b9387
Devamitta <devamitta@sasanarakkha.org>
2023-06-10 06:48:48 +0800

    bash making empty folders

---
+++ mkalldir.sh
@@ -0,0 +1,15 @@
+mkdir "output"
+
+cd output
+
+mkdir "html_tables_dps"
+mkdir "html_tables_sbs"
+mkdir "inflections"
+mkdir "inflections in table"
+mkdir "inflections translit"
+mkdir "patterns"
+mkdir "pickle test"
+touch "all inflections.csv"
+touch "all inflections translit.csv"
+touch "new inflections.csv"
+touch "new inflections translit.csv"
