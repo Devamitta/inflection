344a7643de39ac72d0f856b3edb36f33e11af4c0
Devamitta <devamitta@sasanarakkha.org>
2023-06-15 13:44:14 +0800

    change path of kind SBS to dps-full

--- inflection_generator/cli.py
+++ inflection_generator/cli.py
@@ -26,7 +26,8 @@ def generate_inflections(args: argparse.Namespace) -> None:
     if kind is Kind.DPS:
         csv_file = settings.DPS_DIR/"spreadsheets"/"dps-full.csv"
     elif kind is Kind.SBS:
-        csv_file = settings.DPS_DIR/"word-frequency"/"csv-for-examples"/f"{args.class_file_name}-class.csv"
+        # csv_file = settings.DPS_DIR/"word-frequency"/"csv-for-examples"/f"{args.class_file_name}-class.csv"
+        csv_file = settings.DPS_DIR/"spreadsheets"/"dps-full.csv"
     data, headwords = modules.create_data_frame(csv_file)
 
     modules.test_for_missing_stem_and_pattern(data)
